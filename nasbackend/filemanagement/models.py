2b3513fb0ee8a46acbb74a869cbfbe6d9a818a66
Maximin Joshua <maximinusjoshus@gmail.com>
2024-11-13 09:11:12 -0600

    added file and folder schema

--- nasbackend/filemanagement/models.py
+++ nasbackend/filemanagement/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Folders(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    parent_folder_id = models.ForeignKey('self', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now = True)
+    created_by = models.ForeignKey(User, on_delete=models.RESTRICT)
+
+    class Meta:
+        db_table = 'folders'
+
+class Files(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    parent_folder_id = models.ForeignKey(Folders, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now = True)
+    created_by = models.ForeignKey(User, on_delete=models.RESTRICT)
+
+    class Meta:
+        db_table = 'files'
--- nasbackend/nasbackendproject/settings.py
+++ nasbackend/nasbackendproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "filemanagement",
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,13 @@ WSGI_APPLICATION = "nasbackendproject.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nasbackenddb',
+        'USER': 'root',
+        'PASSWORD': 'furball',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
