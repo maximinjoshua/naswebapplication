5972f7c4e632faca124e28e1f8804dca91a60480
Maximin Joshua <maximinusjoshus@gmail.com>
2024-11-17 08:51:58 -0600

    added views for register and login

--- nasbackend/filemanagement/views.py
+++ nasbackend/filemanagement/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET, require_POST
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.db import IntegrityError, transaction
 
-# Create your views here.
+from django.contrib.auth.models import User
+from .models import Profile
+import json
+
+@require_POST
+def register(request):
+    with transaction.atomic():
+        data = json.loads(request.body)
+        print(data, "Data")
+        user = User.objects.create_user(**data)
+        if user:
+            Profile.objects.create(user = user, user_level = 2)
+        return HttpResponse("Registered successfully")
+
+@require_POST
+def login(request):
+    data = json.loads(request.body)
+    user = authenticate(request, **data)
+    if user:
+        return HttpResponse("Login Successful")
+    else:
+        return HttpResponse("No such user exists")
+
+@require_POST
+def upload_files(request):
+
+    print(dir(request))
+    print(request.FILES)
+    # I can assume now that only GET or POST requests make it this far
+    # ...
+    return HttpResponse("Response sent")
