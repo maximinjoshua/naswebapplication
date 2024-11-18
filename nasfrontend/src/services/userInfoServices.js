2ef7538ca33cc5f0295681bfaee498f1fd337d60
Maximin Joshua <maximinusjoshus@gmail.com>
2024-11-17 20:58:08 -0600

    profile info service call

---
+++ nasfrontend/src/services/userInfoServices.js
@@ -0,0 +1,8 @@
+import { baseServices } from "./BaseServiceCalls"
+
+const getProfileInfo = async(params) => {
+    const response = await baseServices.getData(`getuserprofile?user_id=${params.user_id}`)
+    return response
+}
+
+export const userInfoServices = {getProfileInfo}
